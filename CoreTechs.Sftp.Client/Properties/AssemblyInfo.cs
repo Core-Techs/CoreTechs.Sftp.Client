96ae9eea92a62516c121c64444ca40b4fb48d7f3
robert sloyer <bsloyer@core-techs.net>
2014-03-12 11:22:59 -0400

    Add assembly meta data describing company name, etc.  Rev'd version to 1.0.0.1

--- CoreTechs.Sftp.Client/Properties/AssemblyInfo.cs
+++ CoreTechs.Sftp.Client/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CoreTechs.Sftp.Client")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Secure FTP client library.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("CoreTechs, Inc.")]
 [assembly: AssemblyProduct("CoreTechs.Sftp.Client")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © CoreTechs, Inc. 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
